c89b918c47ff765396f54a5de677ea055053b496
tokuhirom <tokuhirom@gmail.com>
2012-08-10 19:27:27 +0900

    escape single quote

--- test/test.js
+++ test/test.js
@@ -8,4 +8,5 @@ assert.equal(al.autolink("http://google.com/#yay"), "<a href='http://google.com/
 assert.equal(al.autolink("http://google.com/ yay!"), "<a href='http://google.com/'>http://google.com/</a> yay!");
 assert.equal(al.autolink("foo > @yappo heh."), 'foo &gt; @yappo heh.');
 assert.equal(al.autolinkTwitter("http://mixi.jp @yappo #foo hoge"), "<a href='http://mixi.jp'>http://mixi.jp</a> <a href='http://twitter.com/yappo'>@yappo</a> <a href='https://twitter.com/#!/search/?q=%23foo'>#foo</a> hoge");
+assert.equal(al.autolink("'"), '&#39;');
 
